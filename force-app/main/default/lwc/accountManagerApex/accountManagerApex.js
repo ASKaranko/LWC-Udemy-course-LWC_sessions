41445791ec425699997d277fdfaa07a5eec399b8
Karanko Andrei <andr.karanko@gmail.com>
2021-10-07 18:27:28 +0300

    Imperative call to apex method

--- force-app/main/default/lwc/accountManagerApex/accountManagerApex.js
+++ force-app/main/default/lwc/accountManagerApex/accountManagerApex.js
@@ -2,7 +2,10 @@ import { LightningElement, wire } from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountManager.getAccounts';
 
 export default class AccountManagerApex extends LightningElement {
-    @wire(getAllAccounts)
+    // @wire(getAllAccounts)
+    // accounts;
+
+    numberOfRecords;
     accounts;
 
     get response() {
@@ -10,5 +13,21 @@ export default class AccountManagerApex extends LightningElement {
             return true;
         }
         return false;
+    }
+    
+    numberOfAccountsChangeHandler(event) {
+        this.numberOfRecords = event.target.value;
+    }
+
+    getAccountsHandler() {
+        if (this.numberOfRecords) {
+            getAllAccounts({numberOfRecords: +this.numberOfRecords})
+                .then(response => {
+                    this.accounts = response;
+                })
+                .catch(error => {
+                    console.log(error.body.message);
+                })
+        }
     }
 }
