28972eaf8322cdfbf4a8affc7f0df6f01eb4e2f8
Karanko Andrei <andr.karanko@gmail.com>
2021-10-11 18:47:58 +0300

    Implemented car search form and car result form, added events

--- force-app/main/default/lwc/carSearch/carSearch.js
+++ force-app/main/default/lwc/carSearch/carSearch.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class CarSearch extends LightningElement {}
+export default class CarSearch extends LightningElement {
+    carTypeId;
+    
+    constructor() {
+        super();
+        this.carTypeId = '';
+    }
+
+    carTypeSelectHandler(event) {
+        this.carTypeId = event.detail;
+    }
+}
